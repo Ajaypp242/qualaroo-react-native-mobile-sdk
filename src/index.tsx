a40167848287451dff1a0f12c4af70aec110aa67
Ajaypp242 <ajay@proprofs.com>
2021-09-09 18:53:29 +0530

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { NativeModules } from 'react-native';
 
 type QualarooReactNativeMobileSdkType = {
-  multiply(a: number, b: number): Promise<number>;
 };
 
 const { QualarooReactNativeMobileSdk } = NativeModules;
